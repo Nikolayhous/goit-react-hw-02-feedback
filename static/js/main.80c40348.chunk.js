c75c1421e44d9a53345528af344f2634cdfae236
Николай Гребенюк <Yanukhous@ukr.net>
2021-10-04 17:43:36 +0300

    Updates

--- static/js/main.80c40348.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{2:function(e,t,c){"use strict";c.r(t);c(0),c(4)}},[[2,1,2]]]);
-//# sourceMappingURL=main.80c40348.chunk.js.map
