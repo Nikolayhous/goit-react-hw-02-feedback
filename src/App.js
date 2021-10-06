9ce5fe9b500bcc51ba8c464eef2ad0f75b9e9e63
Николай Гребенюк <Yanukhous@ukr.net>
2021-10-06 22:14:25 +0300

    зробив реексрт джс компонетів

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Component } from 'react';
-import Container from './components/Container/Container';
-import Section from './components/Section/Section';
-import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
-import Statistics from './components/Statistics/Statistics';
+import Container from './components/Container';
+import Section from './components/Section/';
+import FeedbackOptions from './components/FeedbackOptions/';
+import Statistics from './components/Statistics/';
 
 class App extends Component {
     state = {
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Notification from '../Notification/Notification';
+import Notification from '../Notification';
 import s from './Statistics.module.css';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
